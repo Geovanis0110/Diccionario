e79c590d9743a03339e29e690431dbf64f24ee30
lazaro <lazaronoelg@gmail.com>
2023-03-08 16:28:08 -0500

    Complete paint-word.directive.ts functionality and paint the possible orthography error

--- src/app/Directives/paint-word.directive.ts
+++ src/app/Directives/paint-word.directive.ts
@@ -1,9 +1,23 @@
-import {Directive, OnInit} from "@angular/core";
+import {Directive, ElementRef, Input, OnInit, Renderer2} from "@angular/core";
 
 @Directive({
   selector: '[paintWord]'
 })
 export class PaintWordDirective implements OnInit {
+  @Input() posError: Array<string> = [];
+  @Input() currentIndex: number = 0;
+
+  constructor(
+    private elRef: ElementRef,
+    private renderer: Renderer2
+  ) {
+  }
+
   ngOnInit() {
+    this.posError.forEach((item) => {
+      if ((this.currentIndex + 1).toString() === item) {
+        this.renderer.setStyle(this.elRef.nativeElement, 'color', '#e95d0f');
+      }
+    })
   }
 }
